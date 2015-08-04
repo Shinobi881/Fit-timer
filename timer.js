94caf470cc222bb5bd5c67b78097d50272136c4b
TGA-Student <tga.student@hackreactor.com>
2015-08-03 19:27:40 -0700

    Using the flipclock timer

---
+++ timer.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+// var timer = $('.timer').FlipClock({
+//             clockFace: 'HourlyCounter'
+//         });
+  
+  // $(".timer").before("<h2>" + timer + "</h2>");
+
+    // $("button").click(function(){
+    //    timer.setTime(60000);
+    //    timer.start();
+               
+    //   });
+
+    // setInterval(function(){$(".timer").html(timer.getTime();)}, 1000);
+});
+  
+
+
+
+
+
+
+
